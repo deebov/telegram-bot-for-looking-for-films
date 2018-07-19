5d5e60349a65a3a3f9024e5fa5a7fe665f7ea508
Dilshod Turobov <deebov@yandex.com>
2018-07-19 00:08:40 -0700

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TOKEN: '461731945:AAGTwT73_mwd1Pdc8nH_3ApTzv79nrhwyAY',
+  TOKEN: '645479333:AAFMW1muEJmvwJPz1dFi5mUo0VLjdSkfvaY',
   DB_URL: 'mongodb://admin:dadajon22@ds241869.mlab.com:41869/cinema'
 }
